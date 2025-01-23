09127e3f8aec3712559b2b770f830d39b909a2c6
Wes Ostlund <wesost11@gmail.com>
2025-01-22 16:17:43 -0800

    working with query results, readline interface

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const mysql = require('mysql2');
+const readline = require('readline');
 
 const pool = mysql.createPool({
     host: process.env.db_host,
@@ -8,13 +9,54 @@ const pool = mysql.createPool({
     database: process.env.db_database
 });
 
-pool.query('select * from users where id=1;', (err, results) =>{
-    if(err) {
-        console.error('error querying database', err);
-        return;
-    }
-    console.log('result of query: ', results);
+const line = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 });
+
+
+const checkUser = (user) => {
+
+    pool.query('select COUNT(*) as count from users where username = ?', [user], (err, results) =>{ // can also do '${user}' instead of ...?', [user]
+        if(err) {
+            console.error('error querying database', err);
+            line.close();
+            shutdown();
+            return;
+        }
+        console.log('result of query: ', results);
+    
+        if(results[0].count > 0){
+            console.log(`username "${user}" already taken`);
+            // alert('username taken');
+            prompt();
+            // show popup element, do not create new entry to db
+        }
+        else {
+            console.log(`new account created, welcome "${user}"`);
+            line.close();
+            shutdown();
+        }
+    });
+}
+
+
+const prompt = () => {
+    line.question('enter username: ', (user) => {
+        checkUser(user);
+    });
+}
+
+prompt();
+
+
+
+
+
+
+
+
+
 
 // handle shutdown
 const shutdown = () => {
